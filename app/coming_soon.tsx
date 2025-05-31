5f12e1e7854f3c04770296cc8df26e89292585e8
unknown <kk.developer.73@gmail.com>
2025-05-31 18:20:35 +0530

    COming Soon

---
+++ app/coming_soon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComingSoon = () => {
+  return (
+    <div className="w-screen h-screen bg-[url(/assets/education.jpg)] bg-cover">
+      <div className="flex justify-center items-center bg-black w-full h-full opacity-50">
+        <h1 className="text-white text-9xl">We're coming soon</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
