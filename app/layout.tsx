b872507ccee3be411900c7abc1d6779e70245b4e
unknown <kk.developer.73@gmail.com>
2025-06-03 16:55:53 +0530

    refreshed welcome page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,6 @@ export const metadata: Metadata = {
   title: "Kolkata's Top Coaching Institute for CA & Foundation",
   description:
     "ThoughtSpring: 36+ Years of Legacy - Kolkata's Top Coaching Institute for CA, JEE Main+Advanced, Olympiad, Class 8 to 12 and Other Competitive Exams. Join today and embark on a journey to academic excellence.",
-  viewport:
-    "width=device-width, initial-scale=1.0, maximum-scale=1.0 , user-scalable=no",
   keywords:
     "NEET Coaching, JEE Coaching, JEE Advanced Coaching, Coaching for Class 8th, Coaching for Class 9th, Coaching for Class 10th, Coaching for class 11th, Coaching for Class 12th, Olympiad Coaching, Foundation Coaching, Top Coaching for NEET, Top Coaching, Coaching for CA, CA Coaching",
 };
